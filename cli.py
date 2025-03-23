36fea4abce7ea6f6976ddd73df04f9d57a3e5c52
karl5252 <karl.michalik@gmail.com>
2025-03-23 13:40:49 +0100

    first commit

--- cli.py
+++ cli.py
@@ -2,6 +2,30 @@ import typer
 
 app = typer.Typer()
 
+tasks = []
+
+
+@app.command()
+def add_task(task: str):
+    tasks.append(task)
+    typer.echo("Task added.")
+
+
+@app.command()
+def list_tasks():
+    print("Tasks:")
+    for task in tasks:
+        typer.echo(f" - {task}")
+
+
+@app.command()
+def done_task(task: str):
+    try:
+        tasks.remove(task)
+        typer.echo(f"Task {task} done.")
+    except ValueError:
+        typer.echo(f"Task {task} not found.")
+
 
 if __name__ == '__main__':
     app()
